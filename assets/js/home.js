c3ca69dc7dfe87977f51aa1707c840c607e3267b
acusp <acusp.xu@gmail.com>
2017-10-28 16:00:12 +0800

    Modify pages layout

--- assets/js/home.js
+++ assets/js/home.js
@@ -17,83 +17,25 @@ $('#page-loading-progress').progress({
     }
 });
 
-// GitHub
-function updateGitHub(repository) {
-    $('#github-watch img').attr('src', 'https://img.shields.io/github/watchers/' + repository + '.svg?style=social&label=Watch');
-    $('#github-star img').attr('src', 'https://img.shields.io/github/stars/' + repository + '.svg?style=social&label=Star');
-    $('#github-fork img').attr('src', 'https://img.shields.io/github/forks/' + repository + '.svg?style=social&label=Fork');
-};
-var repository = $('meta[name=repository]').attr('content');
-updateGitHub(repository);
-setInterval(function () {
-    updateGitHub(repository);
-}, 15000);
-
-// version
-moment.locale('zh-cn');
-function updateVersion(timestamp) {
-    $('#version img').attr('src', 'https://img.shields.io/badge/%E6%9B%B4%E6%96%B0%E4%BA%8E-' + encodeURIComponent(moment(timestamp).fromNow()) + '-brightgreen.svg');
-};
-var updateAt = $('meta[name=updated_at]').attr('content');
-updateVersion(updateAt);
-setInterval(function () {
-    updateVersion(updateAt);
-}, 15000);
-
-// visit
-function updateVisit(updateProgressBar) {
-    $.getJSON(analyticsAPI.url, {
-        'module': 'API',
-        'method': 'VisitsSummary.getUniqueVisitors',
-        'idSite': analyticsAPI.id,
-        'period': 'day',
-        'date': 'today',
-        'format': 'JSON',
-        'token_auth': analyticsAPI.token
-    }, function (data) {
-        $('#today-visitors img').attr('src', 'https://img.shields.io/badge/%E4%BB%8A%E6%97%A5%E8%AE%BF%E5%AE%A2-' + encodeURIComponent(data.value) + '-brightgreen.svg');
-        if (updateProgressBar) {
-            $('#page-loading-progress').progress('increment');
-        };
-    });
-    $.getJSON(analyticsAPI.url, {
-        'module': 'API',
-        'method': 'Live.getCounters',
-        'idSite': analyticsAPI.id,
-        'lastMinutes': '30',
-        'format': 'JSON',
-        'token_auth': analyticsAPI.token
-    }, function (data) {
-        $('#live-visitors img').attr('src', 'https://img.shields.io/badge/%E5%BD%93%E5%89%8D%E5%9C%A8%E7%BA%BF-' + encodeURIComponent(data[0].visitors) + '-brightgreen.svg');
-        if (updateProgressBar) {
-            $('#page-loading-progress').progress('increment');
-        };
-    });
-};
-updateVisit(true);
-setInterval(function () {
-    updateVisit(false);
-}, 15000);
-
 // search
 $('#search-services').dropdown();
-if (Cookies.get('byr_navi_previous_search_service_option') === undefined || Cookies.get('byr_navi_previous_search_service_option') === '' || $('#' + Cookies.get('byr_navi_previous_search_service_option')).length === 0) {
-    Cookies.set('byr_navi_previous_search_service_option', $('#search-services').val(), { expires: 365 });
+if (Cookies.get('qidian_navi_previous_search_service_option') === undefined || Cookies.get('qidian_navi_previous_search_service_option') === '' || $('#' + Cookies.get('qidian_navi_previous_search_service_option')).length === 0) {
+    Cookies.set('qidian_navi_previous_search_service_option', $('#search-services').val(), { expires: 365 });
 } else {
-    $('#search-services').dropdown('set selected', Cookies.get('byr_navi_previous_search_service_option'));
-};
+    $('#search-services').dropdown('set selected', Cookies.get('qidian_navi_previous_search_service_option'));
+}
 
 $('#search-button').click(function () {
     var service = $('#' + $('#search-services').val());
     var query = $('#search-query').val();
     query = encodeURIComponent(query);
     if (query) {
-        Cookies.set('byr_navi_previous_search_service_option', service.val(), { expires: 365 });
-        window.open('search/?service=' + encodeURIComponent(service.text()) + '&query=' + query + '&next=' + encodeURIComponent(service.attr('data-url') + query + service.attr('data-suffix')), '_blank');
+        Cookies.set('qidian_navi_previous_search_service_option', service.val(), { expires: 365 });
+        window.open(service.attr('data-url') + query + service.attr('data-suffix'), "_blank");
     } else {
         $('#search-div').addClass('error');
         $('#search-query').attr('placeholder', '请输入搜索内容');
-    };
+    }
 });
 
 $('#search-query').click(function () {
@@ -117,16 +59,16 @@ $(window).keyup(function (event) {
         query = encodeURIComponent(query);
         if (query) {
             if ($('#search-query:focus').length > 0) {
-                Cookies.set('byr_navi_previous_search_service_option', service.val(), { expires: 365 });
-                window.open('search/?service=' + encodeURIComponent(service.text()) + '&query=' + query + '&next=' + encodeURIComponent(service.attr('data-url') + query + service.attr('data-suffix')), '_blank');
+                Cookies.set('qidian_navi_previous_search_service_option', service.val(), { expires: 365 });
+                window.open(service.attr('data-url') + query + service.attr('data-suffix'), "_blank");
             } else {
                 $('#search-query').focus().select();
-            };
+            }
         } else {
             $('#search-div').addClass('error');
             $('#search-query').attr('placeholder', '请输入搜索内容').focus();
-        };
-    };
+        }
+    }
 });
 
 $('#search-query').keyup(function (event) {
@@ -134,8 +76,8 @@ $('#search-query').keyup(function (event) {
         if ($('#search-query').val()) {
             $('#search-div').removeClass('error');
             $('#search-query').attr('placeholder', '立即搜索');
-        };
-    };
+        }
+    }
 });
 
 $('.shortcuts .ui.label').each(function () {
@@ -144,13 +86,13 @@ $('.shortcuts .ui.label').each(function () {
         var query = $('#search-query').val();
         query = encodeURIComponent(query);
         if (query) {
-            Cookies.set('byr_navi_previous_search_service_option', service.val(), { expires: 365 });
+            Cookies.set('qidian_navi_previous_search_service_option', service.val(), { expires: 365 });
             $('#search-services').dropdown('set selected', service.val());
-            window.open('search/?service=' + encodeURIComponent(service.text()) + '&query=' + query + '&next=' + encodeURIComponent(service.attr('data-url') + query + service.attr('data-suffix')), '_blank');
+            window.open(service.attr('data-url') + query + service.attr('data-suffix'), "_blank");
         } else {
             $('#search-div').addClass('error');
             $('#search-query').attr('placeholder', '请输入搜索内容');
-        };
+        }
     });
 });
 
